717fb3aad320f929eaba48e24685aa54cc76ed1c
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-04-15 12:59:57 -0400

    start css and js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+alert("hello")
+
+// TODO: ask the user's name.
+
+// TODO: Ask 5 y/n questions.
+let answerOne = prompt("Am I happy?");
+
+// use if / else
+// if 'y' then your are correct
+if(answerOne === 'y' || answerOne === 'yes'){
+  console.log("You are correct.");
+} else {
+  // if 'n' then your are incorrect
+  console.log("You are incorrect.");
+}
+
+// TODO: Give a response with an alert to
+//       notify the use if they are correct.
