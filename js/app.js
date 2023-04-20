9ea17c186e3143ae7de7524770e57935059b45a7
Robert McCreary Mannino <fooyinfighter@gmail.com>
2023-04-20 19:38:24 -0400

    update my bio.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
-
+'use strict'
 // TODO: ask the user's name.
 
 // TODO: Ask 5 y/n questions.
 let answerOne = prompt("Am I happy?");
+let answerOneLower = answerOne.toLowerCase();
 
 // use if / else
 // if 'y' then your are correct
-if(answerOne === 'y' || answerOne === 'yes'){
+if(answerOneLower === 'y' || 
+   answerOneLower === 'yes'){
+  // alert("You are correct")
   console.log("You are correct!");
 } else {
+  // alert("You are incorrect")
   // if 'n' then your are incorrect
   console.log("You are incorrect!");
 }
